436734f66bbf558d9c7b3ad8d414c08eacfa5786
npollender <npollender@live.com>
2020-01-18 21:10:10 -0500

    Added several features to existing activities.

---
+++ app/src/main/java/com/example/usf/ExtraIngredients.java
@@ -0,0 +1,14 @@
+package com.example.usf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExtraIngredients extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_extra_ingredients);
+    }
+}
